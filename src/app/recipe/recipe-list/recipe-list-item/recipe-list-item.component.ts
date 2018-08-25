3cba189f6b112a8d687eb0a9f917364a347c4211
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2018-08-25 16:29:07 -0700

    added recipe-list-element component
    Passed the data from listto element component using  property binding.

--- src/app/recipe/recipe-list/recipe-list-item/recipe-list-item.component.ts
+++ src/app/recipe/recipe-list/recipe-list-item/recipe-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from 'src/app/recipe/recipe.model';
 
 @Component({
   selector: 'app-recipe-list-item',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListItemComponent implements OnInit {
 
+  @Input() recipe : Recipe;
+   
   constructor() { }
 
   ngOnInit() {
