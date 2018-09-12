fd9c70e9c99566c95c5449b5b955f2fef619aa87
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2018-09-11 22:35:58 -0700

    DT - form
    Added report componet to add new report

--- src/app/opac/lookup/report/report.component.ts
+++ src/app/opac/lookup/report/report.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Report } from './report.model';
 
 @Component({
   selector: 'app-report',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
+  @ViewChild('f') rptForm: NgForm;
+  submitted : boolean = false ;
+  rptDTO : Report ;
+
+  defaultRptAbbr : string = 'OPR_ABC_1'//1 way binding, B > F
+  rpt_types = ['BATCH','ON-DEMAND','SYSTEM-GENERATED'];
+  desc : string
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  console.log(this.rptForm);}
+
+
+    onSubmit() {
+      this.submitted = true;
+      console.log(this.rptForm);
+
+      this.rptDTO = new Report();
+      this.rptDTO.name = this.rptForm.value.rptData.rptname;
+      this.rptDTO.abbr = this.rptForm.value.rptData.rptabbr;
+      this.rptDTO.type = this.rptForm.value.rpttype;
+      this.rptDTO.format = this.rptForm.value.format;
+      this.rptDTO.email = this.rptForm.value.email;
+
+      console.log(this.rptDTO);
+  
+      this.rptForm.reset();
+    }
+  
+//All registered controlls needs to be set.
+  setvalue(){
+    const suggestedName = 'ORPHAN AUTH REPORT';
+
+    this.rptForm.setValue({
+      rptData: {
+        rptname: suggestedName,
+        rptabbr: 'OPAC_XYZ_2'
+      },
+      email: 'inylbd@doremon.com',
+      format: 'pdf',
+      rpttype: 'ON-DEMAND',
+      desc : 'This is On-demand report in PDF format.'
+    });
+  }
+
+  patch(){
+    const suggestedName = 'ORPHAN AUTH REPORT';
+
+    this.rptForm.form.patchValue({
+      rptData: {
+        rptname: suggestedName,
+        rptabbr: 'OPAC_XYZ_2'
+      },
+      // email: 'inylbd@doremon.com',
+      // format: 'pdf',
+      // rpttype: 'BATCH'
+    });
+  }
+
+  reset(){
+    this.rptForm.reset();
+    this.submitted = true;
   }
 
 }
---
+++ src/app/opac/lookup/report/report.model.ts
@@ -0,0 +1,10 @@
+export class Report {
+    name : string ;
+    abbr : string ;
+    email: string;
+    type : string;
+    format : string ;
+
+    constructor(){ }
+
+}
